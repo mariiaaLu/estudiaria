cb6acfc4727688eda793795291ee382d53615aa7
mariiaaLu <marialucasb8@gmail.com>
2026-06-11 14:40:36 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const require = createRequire(import.meta.url);
 const pdfParse = require("pdf-parse");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
